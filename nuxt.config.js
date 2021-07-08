6ae860d6bfacda37643feb88cfd2bc2d43c60075
Victor Aro <victor.galves@live.com>
2021-07-08 19:51:55 -0300

    Disabled ssr

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,8 @@
 export default {
+  // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
+  ssr: false,
+
   // Global page headers: https://go.nuxtjs.dev/config-head
-  target: 'static',
   head: {
     title: 'etawellness',
     htmlAttrs: {
