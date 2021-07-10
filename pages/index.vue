e3b03842c22a46fc7a5d15b520978e76ef16b0c5
Victor Aro <victor.galves@live.com>
2021-07-10 18:08:51 -0300

    Removed Inigo :(

--- pages/index.vue
+++ pages/index.vue
@@ -1,60 +1,25 @@
 <template>
   <body>
-    <div class="center">
-      <img @click="click" width="200px" id="inigo" src="images/inigo.png" />
-    </div>
+    <section class="placeholder">
+      <h3 class="center">Website under development.</h3>
+    </section>
   </body>
 </template>
 
 <script>
-var audio = new Audio("music.mp3");
-export default {
-  methods: {
-    click() {
-      audio.play();
-      document.getElementById('inigo').classList.add('animate')
-    },
-  },
-};
+export default {};
 </script>
 
 
 <style scoped>
-body {
-  background-color: rgb(87, 87, 170);
+
+.placeholder{
   display: flex;
   width: 100vw;
   height: 100vh;
 }
-.center {
-  display: flex;
-  flex-direction: column;
-  margin: auto;
-}
-#inigo {
-  margin: auto;
-  border-radius: 100%;
-}
 
-.animate{
-  animation-name: inigo;
-  animation-duration: 3s;
-  animation-iteration-count: infinite;
-  animation-timing-function: linear;
-}
-
-.text {
-  color: white;
-  text-align: center;
-  margin-top: 20px;
-  font-size: 1.3rem;
-}
-@keyframes inigo {
-  from {
-    transform: rotate(0deg);
-  }
-  to {
-    transform: rotate(360deg);
-  }
+.center{
+  margin: auto;
 }
 </style>
